43705e495e4d2d53483b406966016656bc2c5768
CiscG <fgss98@gmail.com>
2026-01-10 20:25:12 -0300

    mexendo com rotas

--- SpringBoot/api/src/main/java/br/com/projetoaula/api/controle/Controle.java
+++ SpringBoot/api/src/main/java/br/com/projetoaula/api/controle/Controle.java
@@ -1,12 +1,20 @@
 package br.com.projetoaula.api.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Controle {
-   
+
+    @GetMapping("")
     public String mensagem(){
         return "Hello World!";
+    }
+
+    @GetMapping("/boasvindas")
+    public String BoasVindas(){
+        return "Seja bem vindo(a)";
     }
 
 }
